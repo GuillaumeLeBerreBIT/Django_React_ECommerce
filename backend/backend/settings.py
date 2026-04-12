2aca51db3591595417346eaff5f9e4552f074ef8
GuillaumeLeBerreBIT <leberreguillaume.glb@gmail.com>
2026-04-12 13:32:51 +0200

    add: modeling data + database setup + static files configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,3 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATIC_FILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
